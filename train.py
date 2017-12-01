2d9188a45108c8466a741a22676a071d307d1a09
alexanderplin <alexanderlin@college.harvard.edu>
2017-11-30 23:12:30 -0500

    wrong file

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ from sklearn.feature_extraction import image
 import scipy.misc
 import natsort
 from keras import layers, models
+from multiprocessing import Pool, TimeoutError
 
 def extract_voxel_data(list_of_dicom_files):
     datasets = [dicom.read_file(f) for f in list_of_dicom_files]
@@ -46,46 +47,48 @@ def extract_segment(segment_id, data, y):
 
     return data[64*row:64*(row+1), 64*col:64*(col+1)], y[64*row:64*(row+1), 64*col:64*(col+1)]
 
-def train_model(segment_id, patient_dicoms_dir, dir_of_snapshots=None):
+def train_model(segment_id, patient_dicoms_dir, dir_of_snapshots):
+    print("model_"+str(segment_id)+" training")
     cur_model = None
     start_patient_index = 0
 
-    if dir_of_snapshots is not None:
-        prev_models = natsort.natsorted(os.listdir(os.path.join(dir_of_snapshots,'segment_'+str(segment_id))), reverse=True)
-        if len(prev_models) is not 0:
-            print('loading model...')
-            cur_model = models.load_model(os.path.join(dir_of_snapshots,'segment_'+str(segment_id),prev_models[0]))
-            start_patient_index = int(prev_models[0].split('v')[1].split('.')[0]) + 1
-            print("Starting at patient: ", str(start_patient_index))
-        else:
-            cur_model = make_model()
+    prev_models = natsort.natsorted(os.listdir(os.path.join(dir_of_snapshots,'segment_'+str(segment_id))), reverse=True)
+    if len(prev_models) is not 0:
+        print('loading model...')
+        cur_model = models.load_model(os.path.join(dir_of_snapshots,'segment_'+str(segment_id),prev_models[0]))
+        start_patient_index = int(prev_models[0].split('v')[1].split('.')[0]) + 1
+        print("Starting at patient: ", str(start_patient_index))
     else:
         cur_model = make_model()
-
+    skipped = []
     for idx, sample in enumerate(patient_dicoms_dir[start_patient_index:], start=start_patient_index):
-        lstFilesDCM = []
-        for dirName, subdirList, fileList in os.walk(sample):
-            for filename in fileList:
-                if ".dcm" in filename.lower():  # check whether the file's DICOM
-                    lstFilesDCM.append(os.path.join(dirName,filename))
+        try:
+            lstFilesDCM = []
+            for dirName, subdirList, fileList in os.walk(sample):
+                for filename in fileList:
+                    if ".dcm" in filename.lower():  # check whether the file's DICOM
+                        lstFilesDCM.append(os.path.join(dirName,filename))
 
-        data, slices  = extract_voxel_data(lstFilesDCM)
-        # k to z voxel mapping
-        k_to_z = {k:v for v,k in enumerate(sorted(slices))}
+            data, slices  = extract_voxel_data(lstFilesDCM)
+            # k to z voxel mapping
+            k_to_z = {k:v for v,k in enumerate(sorted(slices))}
 
-        label_xml = glob.glob(os.path.join(sample,"*.xml"))
-        y = generate_labels_from_xml(label_xml[0], data.shape, k_to_z)
+            label_xml = glob.glob(os.path.join(sample,"*.xml"))
+            y = generate_labels_from_xml(label_xml[0], data.shape, k_to_z)
 
-        data, y = extract_segment(segment_id, data, y)
-        data, y = data.T, y.T
+            data, y = extract_segment(segment_id, data, y)
+            data, y = data.T, y.T
 
-        cur_model.train_on_batch(data.reshape(data.shape[0], data.shape[1], data.shape[2], 1),y.reshape(y.shape[0], y.shape[1], y.shape[2], 1))
-        loss = cur_model.evaluate(data.reshape(data.shape[0], data.shape[1], data.shape[2], 1),y.reshape(y.shape[0], y.shape[1], y.shape[2], 1), data.shape[0])
-        print(loss)
-        if(idx % 50 == 0):
-            print('Saving Model')
-            cur_model.save(os.path.join(dir_of_snapshots,'segment_'+str(segment_id), 'model_'+str(segment_id)+'v'+str(idx)+'.h5'))
-
+            cur_model.train_on_batch(data.reshape(data.shape[0], data.shape[1], data.shape[2], 1),y.reshape(y.shape[0], y.shape[1], y.shape[2], 1))
+            loss = cur_model.evaluate(data.reshape(data.shape[0], data.shape[1], data.shape[2], 1),y.reshape(y.shape[0], y.shape[1], y.shape[2], 1), data.shape[0])
+            print('model_'+str(segment_id) + ' Patient: '+str(idx), loss)
+            if(idx % 50 == 0):
+                print('Saving Model')
+                cur_model.save(os.path.join(dir_of_snapshots,'segment_'+str(segment_id), 'model_'+str(segment_id)+'v'+str(idx)+'.h5'))
+        except:
+            print('SKIPPING PATIENT '+str(idx))
+            continue
+    return skipped
 def init_model_snapshots_dir(dir_of_snapshots):
     if not os.path.exists(dir_of_snapshots):
         os.makedirs(dir_of_snapshots)
@@ -106,13 +109,22 @@ def make_model():
               metrics=['accuracy'])
     return model
 
-primary = os.path.join(".","data","LIDC-IDRI")
-patient_folders = [os.path.join(primary,f) for f in os.listdir(primary) if os.path.isdir(os.path.join(primary, f))]
+if __name__ == '__main__':
+    primary = os.path.join(".","data","LIDC-IDRI")
+    patient_folders = [os.path.join(primary,f) for f in os.listdir(primary) if os.path.isdir(os.path.join(primary, f))]
 
-patient_dicoms_dir = []
-for sample in patient_folders:
-    len_list = [len(os.listdir(os.path.join(sample,fil,os.listdir(os.path.join(sample, fil))[0]))) for fil in os.listdir(sample)]
-    correct_file = os.path.join(sample, os.listdir(sample)[len_list.index(max(len_list))])
-    patient_dicoms_dir.append(os.path.join(correct_file,os.listdir(correct_file)[0]))
+    patient_dicoms_dir = []
+    for sample in patient_folders:
+        len_list = [len(os.listdir(os.path.join(sample,fil,os.listdir(os.path.join(sample, fil))[0]))) for fil in os.listdir(sample)]
+        correct_file = os.path.join(sample, os.listdir(sample)[len_list.index(max(len_list))])
+        patient_dicoms_dir.append(os.path.join(correct_file,os.listdir(correct_file)[0]))
 
-train_model(45, patient_dicoms_dir)
+    init_model_snapshots_dir('model_snapshots')
+
+    #train_model(50,patient_dicoms_dir,'model_snapshots')
+    pool = Pool(processes=10)
+    args = ((i, patient_dicoms_dir[:499], "model_snapshots") for i in range(63))
+    X = pool.starmap_async(train_model, args)
+    pool.close()
+    pool.join()
+    print(X.get())
